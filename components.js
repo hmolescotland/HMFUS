f6e16dad43fe579bba1722cbae020955e4e6ee3e
hmolescotland <tay.hmolescotland@nhs.scot>
2026-04-27 14:55:44 +0100

    Update components.js

--- components.js
+++ components.js
@@ -81,11 +81,19 @@ function getNHSFooter() {
         <p>Tel: 01382 632748</p>
         <a href="mailto:TAY.hmolescotland@nhs.scot">TAY.hmolescotland@nhs.scot</a>
       </div>
+
+
+
+      
       <div>
         <h4>Service Hours</h4>
         <p>Monday – Friday<br>9:00am – 4:00pm</p>
         <p>Closed on public holidays.</p>
       </div>
+
+
+
+      
       <div>
         <h4>Related Services</h4>
         <a href="https://www.nhsinform.scot/illnesses-and-conditions/pregnancy-and-childbirth/losing-a-baby/molar-pregnancy/" target="_blank">NHS Inform</a>
